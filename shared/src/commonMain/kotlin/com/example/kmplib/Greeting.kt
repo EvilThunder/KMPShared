3fae813592a8f1b86dcd6898dfcfa7805bd1ee95
wangshaowei <wangshaowei@corp.netease.com>
2025-05-21 18:13:02 +0800

    feat: 点击事件接口

--- shared/src/commonMain/kotlin/com/example/kmplib/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmplib/Greeting.kt
@@ -1,9 +1,21 @@
 package com.example.kmplib
 
 class Greeting {
+    companion object {
+        var onClickListener: ((Int) -> Unit)? = null
+    }
+
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
         return "Hello, ${platform.name}!"
+    }
+
+    fun invokeClick() {
+        onClickListener?.invoke(20)
+    }
+
+    fun setOnClick(onClick: (Int) -> Unit) {
+        onClickListener = onClick
     }
 }
